5ecca1b055c4856f0d1590de0a2a16de445ca81c
Sean D <seandow382@gmail.com>
2019-11-08 16:33:32 -0800

    implemented an ride entry for display in the list of available rides

--- src/BrowseRides.js
+++ src/BrowseRides.js
@@ -1,9 +1,57 @@
 import React, { Component } from 'react';
+import {
+    View,
+    Text,
+    FlatList,
+    StyleSheet,
+} from 'react-native';
 
-class BrowseRides extends Component {
+import PropTypes from 'prop-types'
+
+export default class BrowseRides extends Component {
+
     render() {
-        return {
-
-        };
+        return (
+            <View style={{padding: 10}}>
+                <RideEntry />
+            </View>
+        );
     }
 }
+
+class RideEntry extends Component {
+
+    render() {
+        return (
+            <View style={styles.container}>
+                <Text>Driver Name: {this.props.name}</Text>
+                <Text>License Plate: {this.props.licensePlate}</Text>
+                <Text>Location: {this.props.location}</Text>
+                <Text>Pickup Time: {this.props.pickupTime}</Text>
+            </View>
+        );
+    }
+}
+
+RideEntry.propTypes = {
+    name:           PropTypes.string,
+    licensePlate:   PropTypes.string,
+    location:       PropTypes.string,
+    pickupTime:     PropTypes.string,
+}
+
+RideEntry.defaultProps = {
+    name:           "default name",
+    licensePlate:   "default license plate",
+    location:       "default location",
+    pickupTime:     "default pickup time",
+}
+
+const styles = StyleSheet.create({
+    container: {
+        borderRadius: 4,
+        borderWidth: 0.5,
+        borderColor: '#d6d7da'
+    }
+});
+
