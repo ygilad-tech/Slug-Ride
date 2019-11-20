6c595a3d5c80be7821338a9f6cec414df02c3e69
Neel Jain <Njain6@ucsc.edu>
2019-11-20 09:46:57 -0800

    Update LoginPage.js

--- src/LoginPage.js
+++ src/LoginPage.js
@@ -50,82 +50,47 @@ export default class LoginPage extends Component {
     try {
       firebase.auth().signInWithEmailAndPassword(email, pass);
       firebase.auth().onAuthStateChanged(user => {
-         alert(user.email);
+         alert("Hello " + user.email);
       })
     } catch (error) {
       console.log(error.toString(error));
     }
   };
 	render() {
-    return (
-      <Container style={styles.container}>
-        <Form>
-          <Item floatingLabel>
-            <Label>Email</Label>
-            <Input autoCapitalize="none" autoCorrect={false} 
-            onChangeText={(email) => this.setState({email})}/>
-          </Item>
-          <Item floatingLabel>
-            <Label>Password</Label>
-            <Input
-              secureTextEntry={true}
-              autoCapitalize="none"
-              autoCorrect={false}
-              onChangeText={(pass) => this.setState({pass})}
-            />
-          </Item>
-          <Button full rounded 
-          style={{ marginTop: 20 }}
-          onPress={() => this.SignIn(this.state.email,
-          this.state.pass)}>
-            <Text>Sign In</Text>
-          </Button>
-          <Button full rounded 
-          style={{ marginTop: 20 }}
-          onPress={() => this.SignUp(this.state.email,
-          this.state.pass)}>
-            <Text>Sign Up</Text>
-          </Button>
-        </Form>
-      </Container>
-    );
-  }
- //    render() {
-	// 	const {navigate} = this.props.navigation; /* Allows navigation to other screens */
- //        return (
- //            //<View style={{alignItems: 'center', top: 50}}>
- //            //    <Text>Enter your email and password below:</Text>
- //            //</View>
- //            <View style = {{padding: 10}}>
-	//             <TextInput 
-	// 				style = {{height: 20, paddingVertical: 0}}
-	// 				underlineColorAndroid = "transparent"
-	//            		placeholder = "Enter your UCSC email here"
-	//            		onChangeText={(email) => this.setState({email})}
-	//           		value={this.state.email}
-	//            	/>
-	//             <TextInput 
-	// 				style = {{height: 20, paddingVertical: 0}}
-	// 				underlineColorAndroid = "transparent"
-	//            		placeholder = "Enter your password here"
-	//            		onChangeText={(pass) => this.setState({pass})}
-	//           		value={this.state.pass}
-	//            	/>
-	//            	<Button 
-	//             	title = "Done!"
-	//             	onPress={() => navigate('BrowseRidesPage')}
-	//             />
-	//         </View>
- // ec660b3dd476189230af54ffe6734fb9947e746d
-	            
-            
-    //         ///	<Text style={{padding: 10, fontSize: 42}}>
-    //       	//	{this.state.text.split(' ').map((word) => word && '🍕').join(' ')}
-    //     	//	</Text>
-
-            
-    //     );
-    // }
+		const {navigate} = this.props.navigation;
+		return (
+		  <Container style={styles.container}>
+		    <Form>
+		      <Item floatingLabel>
+		        <Label>Email</Label>
+		        <Input autoCapitalize="none" autoCorrect={false} 
+		        onChangeText={(email) => this.setState({email})}/>
+		      </Item>
+		      <Item floatingLabel>
+		        <Label>Password</Label>
+		        <Input
+		          secureTextEntry={true}
+		          autoCapitalize="none"
+		          autoCorrect={false}
+		          onChangeText={(pass) => this.setState({pass})}
+		        />
+		      </Item>
+		      <Button full rounded 
+		      style={{ marginTop: 20 }}
+		      onPress={() => {this.SignIn(this.state.email,
+		      this.state.pass); navigate('BrowseRidesPage')}}>
+		        <Text>Sign In</Text>
+		      </Button>
+		      <Button full rounded 
+		      style={{ marginTop: 20 }}
+		      onPress={() => this.SignUp(this.state.email,
+		      this.state.pass)}>
+		        <Text>Sign Up</Text>
+		      </Button>
+		    </Form>
+		  </Container>
+		);
+  	}
 }
 const styles = StyleSheet.create({
   container: {
