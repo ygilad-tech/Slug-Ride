cd9194dbfb19411b3c0559f95781c4438f7cc285
ygilad-tech <56288047+ygilad-tech@users.noreply.github.com>
2019-12-09 22:42:44 -0800

    undid previous commit

--- react-native/src/CreateRide.js
+++ react-native/src/CreateRide.js
@@ -26,35 +26,8 @@ export default class CreateRide extends Component {
         };
     }
 
-    async getUserData() {
-        const user = firebaseApp.auth().currentUser;
-
-        var profRef = db.collection('Profiles');
-
-        let profDoc = profRef.where('usr', '==', user.uid).get()
-            .then(snapshot => {
-                if (snapshot.empty) {
-                    return;
-                }
-                snapshot.forEach(doc => {
-                    var d = doc.data();
-                    this.setState({
-                        DriverName: d.firstName + " " + d.lastName,
-                    });
-                });
-            })
-            .catch(function (error) {
-                
-            });
-
-        return;
-    }
-
     render() {
         const {navigate} = this.props.navigation
-        const user = firebaseApp.auth().currentUser;
-        this.getUserData();
-        var dName = this.state.DriverName;
 
         return (
             <View style={{flex: 1}}>
@@ -68,7 +41,7 @@ export default class CreateRide extends Component {
                         <TextInput
                             style={styles.textInputStyle}
                             underlineColorAndroid = "transparent"
-                            placeholder = 'dname'
+                            placeholder = 'Name'
                             onChangeText={(DriverName) =>  this.setState({DriverName})}
                             value = {this.state.DriverName}
                         />
